67803e0876db25d40be97b22e16f46f37b3428fd
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-02 18:46:21 +0800

    feat: add ability to reload quick nav buttons

--- src/reloadQuickNavigator.js
+++ src/reloadQuickNavigator.js
@@ -4,10 +4,10 @@ export default () => {
     const quickNav = document.querySelector("#quickNav");
     const quickNavBtnArray = Array.from(quickNav.children);
 
+    const currentIndex = getCurrentIndex();
+
     quickNavBtnArray.forEach((quickNavBtn) => {
         quickNavBtn.classList.remove('bg-gray-700');
     })
-
-    const currentIndex = getCurrentIndex();
     quickNavBtnArray[currentIndex].classList.add('bg-gray-700');
 }
--- src/scrollPrev.js
+++ src/scrollPrev.js
@@ -1,7 +1,9 @@
 import { getCurrentIndex, setCurrentIndex } from "./sharedState";
 import ReloadImage from './reloadImage';
+import ReloadQuickNavigator from "./reloadQuickNavigator";
 
 export default () => {
     setCurrentIndex(getCurrentIndex() - 1);
     ReloadImage();
+    ReloadQuickNavigator();
 }
