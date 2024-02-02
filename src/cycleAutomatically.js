b93f8d05185e1ea838908d9e9bc2c939f6924f20
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-02 17:59:13 +0800

    feat: add ability to pause automatic scrolling on hover

--- src/cycleAutomatically.js
+++ src/cycleAutomatically.js
@@ -8,6 +8,17 @@ export default () => {
         autoScrollIntervalId = setInterval(ScrollNext, INTERVAL);
     }
 
+    const stopAutoScroll = () => clearInterval(autoScrollIntervalId);
+
     startAutoScroll();
 
+    const prevBtn = document.querySelector("#prevBtn");
+    const nextBtn = document.querySelector("#nextBtn");
+    const quickNav = document.querySelector("#quickNav");
+    const imageDisplay = document.querySelector("#imageDisplay");
+
+    [prevBtn, nextBtn, quickNav, imageDisplay].forEach((element) => {
+        element.addEventListener("mouseenter", stopAutoScroll);
+        element.addEventListener("mouseleave", startAutoScroll);
+    })
 }
